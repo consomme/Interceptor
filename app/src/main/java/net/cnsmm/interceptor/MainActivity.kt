ffd8dfec35f552212fbffa80029dde87a509f5a9
consomme <consomme72@gmail.com>
2017-12-02 22:30:49 +0900

    use ShortcutManagerCompat

--- app/src/main/java/net/cnsmm/interceptor/MainActivity.kt
+++ app/src/main/java/net/cnsmm/interceptor/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.support.v4.content.pm.ShortcutInfoCompat
+import android.support.v4.content.pm.ShortcutManagerCompat
+import android.support.v4.graphics.drawable.IconCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -30,27 +33,28 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ApplicationAdapter(list) { appInfoModel ->
             createShortcut(appInfoModel)
+
+            Toast.makeText(this,
+                    getString(R.string.create_shortcut_succeeded, appInfoModel.name),
+                    Toast.LENGTH_SHORT).show()
+
+            finish()
         }
         binding.recyclerView.adapter = adapter
     }
 
     private fun createShortcut(appInfoModel: AppInfoModel) {
-        val shortcutIntent = InterceptActivity.createIntent(this, appInfoModel.packageName);
-        shortcutIntent.action = Intent.ACTION_MAIN
-        shortcutIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val intent = InterceptActivity.createIntent(this, appInfoModel.packageName);
+        intent.action = Intent.ACTION_MAIN
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
 
         val iconBitmapDrawable = appInfoModel.icon as BitmapDrawable
-
-        val intent = Intent("com.android.launcher.action.INSTALL_SHORTCUT")
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, appInfoModel.name);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, iconBitmapDrawable.bitmap);
-        sendBroadcast(intent);
-
-        Toast.makeText(this,
-                getString(R.string.create_shortcut_succeeded, appInfoModel.name),
-                Toast.LENGTH_SHORT).show()
-
-        finish()
+        val shortcutInfoCompat = ShortcutInfoCompat.Builder(this, "interceptor")
+                .setShortLabel(appInfoModel.name)
+                .setLongLabel(appInfoModel.name)
+                .setIcon(IconCompat.createWithBitmap(iconBitmapDrawable.bitmap))
+                .setIntent(intent)
+                .build()
+        ShortcutManagerCompat.requestPinShortcut(this, shortcutInfoCompat, null)
     }
 }
